6055877c129f1a12044b55f0368a3c41c4bfb92c
Thomas Auffredou <tauffredou@xebia.fr>
2018-03-27 00:21:31 +0200

    Add connector

--- lib/CI/connector.rb
+++ lib/CI/connector.rb
@@ -1,55 +1,74 @@
-require "kafka"
-require "socket"
-
+require 'kafka'
+require 'socket'
+require 'json'
 module CI
+  class Connector
 
-class Connector
+    attr_accessor :logger
 
-    def initialize(client_id, group_id, bootstrap_servers = 'localhost:9092', topic = 'github' )
-        @client_id = client_id
-        @group_id = group_id
-        @topic = topic
-        @bootstrap_servers = bootstrap_servers
-        @subscribers = {}
+    def initialize(client_id, group_id, bootstrap_servers = 'localhost:9092', topic = 'github')
+      @client_id = client_id
+      @group_id = group_id
+      @topic = topic
+      @bootstrap_servers = bootstrap_servers
+      @subscribers = {}
+
+      @logger = defaultLogger
     end
 
-    def on(event,&proc)
-        ( subscribers[event] ||= [] ) << proc
+    def defaultLogger
+      logger = Logger.new(STDOUT)
+      logger.level = Logger::INFO
+      logger
+    end
+
+    def on(event, proc)
+      (@subscribers[event] ||= []) << proc
     end
 
     def self.fromEnv
-        self.new(
-            client_id: ENV.fetch("KAFKA_CLIENT_ID",Socket.gethostname),
-            # By default, use the hostname as consumer group
-            group_id: ENV.fetch("KAFKA_CONSUMER_GROUP",Socket.gethostname)
-            bootstrap_servers:  ENV.fetch("KAFKA_BROKERS","localhost:9092").split(","),
-            topic: ENV.fetch("KAFKA_TOPIC","github"),
-           )
+      new(ENV.fetch('KAFKA_CLIENT_ID', Socket.gethostname),
+          # By default, use the hostname as consumer group
+          ENV.fetch('KAFKA_CONSUMER_GROUP', Socket.gethostname),
+          ENV.fetch('KAFKA_BROKERS', 'localhost:9092').split(','),
+          ENV.fetch('KAFKA_TOPIC', 'github'))
     end
 
     # Start listening for events
     def start
+      kafka = Kafka.new(
+        seed_brokers: @bootstrap_servers,
+        client_id: @client_id,
+        socket_timeout: 20,
+        logger: @logger
+      )
 
-        logger = Logger.new(STDOUT)
+      @consumer = kafka.consumer(group_id: @group_id)
+      @consumer.subscribe(@topic)
 
-        kafka = Kafka.new(
-            seed_brokers: @bootstrap_servers,
-            client_id: @client_id,
-            socket_timeout: 20,
-            logger: logger,
-        )
+      @consumer.each_message do |message|
+        data = JSON.parse message.value
 
-        consumer = kafka.consumer(group_id: @group_id)
-        consumer.subscribe(topic)
-
-        consumer.each_message(topic: topic) do |message|
-            subscribers[message['type']].each do |subscriber|
-                subscriber.call message
-            end
+        @logger.debug data
+        if data.has_key? 'type'
+          @subscribers[data['type']].each do |subscriber|
+            subscriber.call data
+          end
         end
-
+      end
     end
 
-end
+    def stop
+      @consumer.stop
+    end
+    # Events
 
+    def onPullRequestOpened(&block)
+      on('pull_request.opened', block)
+    end
+
+    def onPullRequestClosed(&block)
+      on('pull_request.closed', block)
+    end
+  end
 end
--- lib/CI/connector/version.rb
+++ lib/CI/connector/version.rb
@@ -1,5 +1,5 @@
 module CI
   module Connector
-    VERSION = "0.1.0"
+    VERSION = '0.1.0'.freeze
   end
 end
