3777a9f7671156031e375eb0da1e785f7a380f4b
Thomas Auffredou <tauffredou@xebia.fr>
2019-01-24 15:46:08 +0100

    refactor: beautify

--- lib/CI/connector.rb
+++ lib/CI/connector.rb
@@ -2,71 +2,85 @@ require 'kafka'
 require 'socket'
 require 'json'
 module CI
-  class Connector
+	class Connector
 
-    attr_accessor :logger
+		attr_accessor :logger
 
-    def initialize(client_id, group_id, bootstrap_servers = 'localhost:9092')
-      @client_id = client_id
-      @group_id = group_id
-      @bootstrap_servers = bootstrap_servers
-      @subscribers = Hash.new()
+		def initialize(client_id, group_id, bootstrap_servers = 'localhost:9092')
+			@client_id = client_id
+			@group_id = group_id
+			@bootstrap_servers = bootstrap_servers
+			@subscribers = {}
 
-      @logger = defaultLogger
-    end
+			@logger = default_logger
+		end
 
-    def defaultLogger
-      logger = Logger.new(STDOUT)
-      logger.level = Logger::INFO
-      logger
-    end
+		def default_logger
+			logger = Logger.new(STDOUT)
+			logger.level = Logger::INFO
+			logger
+		end
 
-    def on(topic, &proc)
-      (@subscribers[topic] ||= []) << proc
-    end
+		def on(event, &proc)
+			(@subscribers[event] ||= []) << proc
+		end
 
-    def self.from_env
-      new(ENV.fetch('KAFKA_CLIENT_ID', Socket.gethostname),
-          # By default, use the hostname as consumer group
-          ENV.fetch('KAFKA_CONSUMER_GROUP', Socket.gethostname),
-          ENV.fetch('KAFKA_BROKERS', 'localhost:9092').split(','))
-    end
+		# from_env return a kafka client from environment variables
+		# @return [::CI:Connector]
+		def self.from_env
+			new(ENV.fetch('KAFKA_CLIENT_ID', Socket.gethostname),
+					# By default, use the hostname as consumer group
+					ENV.fetch('KAFKA_CONSUMER_GROUP', Socket.gethostname),
+					ENV.fetch('KAFKA_BROKERS', 'localhost:9092').split(','))
+		end
 
-    # Start listening for events
-    def start
-      kafka = Kafka.new(
-        seed_brokers: @bootstrap_servers,
-        client_id: @client_id,
-        socket_timeout: 20,
-        logger: @logger
-      )
+		# fromEnv is : use from_env
+		# @deprecated
+		# @return [::CI:Connector]
+		def self.fromEnv
+			@logger.warn "fromEnv method is deprecated, use from_env"
+			self.from_env
+		end
 
-      @consumer = kafka.consumer(group_id: @group_id)
+		# Start listening for events
+		def start
 
-      @subscribers.each do |key, value|
-        @logger.info "Subscribe topic #{key}"
-        @consumer.subscribe(key)
-      end
+			trap("TERM", &method(:stop))
+			trap("INT", &method(:stop))
 
-      @consumer.each_message do |message|
-        @logger.debug message
-        begin
-          data = JSON.parse message.value
-        rescue
-          data = :null
-          @logger.error "Invalid message #{message.value}"
-        end
-        if data != :null
-          @subscribers[message.topic].each do |subscriber|
-            subscriber.call data
-          end
-        end
-      end
-    end
+			kafka = Kafka.new(
+				seed_brokers: @bootstrap_servers,
+				client_id: @client_id,
+				socket_timeout: 20,
+				logger: @logger
+			)
 
-    def stop
-      @consumer.stop
-    end
+			@consumer = kafka.consumer(group_id: @group_id)
 
-  end
+			@subscribers.each do |key, _|
+				@logger.info "Subscribe topic #{key}"
+				@consumer.subscribe(key)
+			end
+
+			@consumer.each_message do |message|
+				@logger.debug message
+				begin
+					data = JSON.parse message.value
+				rescue
+					data = :null
+					@logger.error "Invalid message #{message.value}"
+				end
+				if data != :null
+					@subscribers[message.topic].each do |subscriber|
+						subscriber.call data
+					end
+				end
+			end
+		end
+
+		def stop
+			@consumer.stop
+		end
+
+	end
 end
